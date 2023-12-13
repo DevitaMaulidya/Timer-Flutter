75240b12e70ae815cc0ac69fdd4558e97e188f2c
DevitaMaulidya <dvtmaulidya@gmail.com>
2023-12-13 12:36:07 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pertemuan_9
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/latihan/pertemuan_9/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.latihan.pertemuan_9
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
